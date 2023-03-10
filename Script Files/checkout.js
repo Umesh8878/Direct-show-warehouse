662dbafc561ac494b96587375d8e19cb2c20e4fc
Umesh <yomyouronlinemart@gmail.com>
2023-03-10 23:20:49 +0530

    done

--- Script Files/checkout.js
+++ Script Files/checkout.js
@@ -1,7 +1,6 @@
 function submit(){
     let orderConfirm = document.getElementById("order-message");
     orderConfirm.textContent = "Your address successfully added";
-    localStorage.clear();
     setTimeout(()=>{
         location.replace("payment.html");
     },2000);
